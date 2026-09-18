e1655f5df172b2b86ba2bc8f7c0b75b9575c109f
khenlumindas <khen.lumindas@csucc.edu.ph>
2026-09-18 10:03:50 +0800

    adffg

--- main.dart
+++ main.dart
@@ -1,10 +1,46 @@
 import 'dart:io';
 
-void main(){
-  late String name;
+void main() {
+  int evenCount = 0;
+  int oddCount = 0;
 
-  stdout.write("Enter your name: ");
-  name = stdin.readLineSync()!;
+  for (int i = 1; i <= 5; i++) {
+    stdout.write("Enter number $i: ");
+    int number = int.parse(stdin.readLineSync()!);
 
-  print("Hi!, $name");
+    // Check even or odd
+    if (number % 2 == 0) {
+      evenCount++;
+      print("$number is EVEN");
+    } else {
+      oddCount++;
+      print("$number is ODD");
+    }
+
+    // Check divisibility
+    if (number % 2 == 0) {
+      print("  Divisible by 2: YES");
+    } else {
+      print("  Divisible by 2: NO");
+    }
+
+    if (number % 3 == 0) {
+      print("  Divisible by 3: YES");
+    } else {
+      print("  Divisible by 3: NO");
+    }
+
+    if (number % 5 == 0) {
+      print("  Divisible by 5: YES");
+    } else {
+      print("  Divisible by 5: NO");
+    }
+
+    print("");
+  }
+
+  // Display total
+  print("===== RESULT =====");
+  print("Total Even Numbers: $evenCount");
+  print("Total Odd Numbers: $oddCount");
 }
