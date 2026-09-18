6484c1ae3e234f498cee557f27a71a9a171226f0
wendypasadillo-dot <wendypasadillo@csucc.edu.ph>
2026-09-18 10:07:09 +0800

    group1

--- main.dart
+++ main.dart
@@ -1,10 +1,42 @@
 import 'dart:io';
 
-void main(){
-  late String name;
+void calculateTotal() {
+  double total = 0;
 
-  stdout.write("Enter your name: ");
-  name = stdin.readLineSync()!;
+  for (int i = 1; i <= 5; i++) {
+    stdout.write("Enter price of product $i: ₱");
+    double price = double.parse(stdin.readLineSync()!);
+    total += price;
+  }
 
-  print("Hi!, $name");
+  print("\nTotal: ₱${total.toStringAsFixed(2)}");
+
+  if (total >= 1000) {
+    double discount = total * 0.10;
+    double finalTotal = total - discount;
+
+    print("Discount (10%): ₱${discount.toStringAsFixed(2)}");
+    print("Final Total: ₱${finalTotal.toStringAsFixed(2)}");
+  } else {
+    print("No discount.");
+    print("Final Total: ₱${total.toStringAsFixed(2)}");
+  }
+}
+
+void calculateFactorial() {
+  stdout.write("\nEnter a positive integer: ");
+  int num = int.parse(stdin.readLineSync()!);
+
+  int factorial = 1;
+
+  for (int i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+
+  print("Factorial of $num: $factorial");
+}
+
+void main() {
+  calculateTotal();
+  calculateFactorial();
 }
